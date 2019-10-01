accd406cbaf9c2851c349c3c1d835386fb6c4d87
Matthias Splieth <matthias@splieth.com>
2019-10-01 14:44:19 +0200

    Add tests for non-main functions

--- config-validator_test.go
+++ config-validator_test.go
@@ -59,3 +59,110 @@ func TestValidate(t *testing.T) {
 	})
 
 }
+
+func TestProbe_IsValid(t *testing.T) {
+	t.Run("valid probe", func(t *testing.T) {
+		validProbe := Probe{Url: "/internal/health"}
+
+		result := validProbe.IsValid()
+
+		assert.Nil(t, result)
+	})
+	t.Run("invalid probe", func(t *testing.T) {
+		validProbe := Probe{}
+
+		result := validProbe.IsValid()
+
+		assert.NotNil(t, result)
+	})
+}
+
+func TestConfigElement_IsValid(t *testing.T) {
+	t.Run("valid config element", func(t *testing.T) {
+		validConfigElement := ConfigElement{
+			Name:    "some-backend",
+			Context: []string{"/context"},
+			BackendConfiguration: BackendConfiguration{
+				Backends: []Backend{
+					{
+						Host: "some-backend.some-host.com",
+						Port: 443,
+					},
+				},
+				ConnectTimeout:      1,
+				FirstByteTimeout:    2,
+				BetweenBytesTimeout: 3,
+			},
+			Probe: Probe{Url: "/health"},
+			Local: false,
+		}
+
+		assert.Nil(t, validConfigElement.IsValid(true))
+	})
+
+	t.Run("config element with invalid probe", func(t *testing.T) {
+		invalidConfigElement := ConfigElement{
+			Name:    "some-backend",
+			Context: []string{"/context"},
+			BackendConfiguration: BackendConfiguration{
+				Backends: []Backend{
+					{
+						Host: "some-backend.some-host.com",
+						Port: 443,
+					},
+				},
+				ConnectTimeout:      1,
+				FirstByteTimeout:    2,
+				BetweenBytesTimeout: 3,
+			},
+			Probe: Probe{},
+			Local: false,
+		}
+
+		assert.NotNil(t, invalidConfigElement.IsValid(true))
+	})
+
+	t.Run("config element name missing", func(t *testing.T) {
+		validConfigElement := ConfigElement{
+			Name:    "",
+			Context: []string{"/context"},
+			BackendConfiguration: BackendConfiguration{
+				Backends: []Backend{
+					{
+						Host: "some-backend.some-host.com",
+						Port: 443,
+					},
+				},
+				ConnectTimeout:      1,
+				FirstByteTimeout:    2,
+				BetweenBytesTimeout: 3,
+			},
+			Probe: Probe{Url: "/health"},
+			Local: false,
+		}
+
+		assert.NotNil(t, validConfigElement.IsValid(true))
+	})
+
+	t.Run("config element context missing", func(t *testing.T) {
+		validConfigElement := ConfigElement{
+			Name:    "name",
+			Context: []string{},
+			BackendConfiguration: BackendConfiguration{
+				Backends: []Backend{
+					{
+						Host: "some-backend.some-host.com",
+						Port: 443,
+					},
+				},
+				ConnectTimeout:      1,
+				FirstByteTimeout:    2,
+				BetweenBytesTimeout: 3,
+			},
+			Probe: Probe{Url: "/health"},
+			Local: false,
+		}
+
+		assert.NotNil(t, validConfigElement.IsValid(true))
+	})
+}
